4d54874df22dda0a6581af2aa3c90a9ad0de2333
Gleb <gleb.verbickiy@yandex.ru>
2024-05-13 19:04:59 +0300

    create project

---
+++ src/test/java/com/java/recruitment/RecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
